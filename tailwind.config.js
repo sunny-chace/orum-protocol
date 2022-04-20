a5164291e631c0ae484ed758a92006d045b263c4
Sunny Dhanwani <sunny.dhanwani@joinleadoff.com>
2022-04-20 11:44:53 +0530

    Updated the config file 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx, css}",
-    "./components/**/*.{js,ts,jsx,tsx, css}"
+    "./pages/**/*.{js,ts,jsx,tsx, css, html}",
+    "./components/**/*.{js,ts,jsx,tsx, css, html}"
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
